c92f52651e3cc2ea6920420e2c719ed3ce2c152f
Nick Young <nick.young@auckland.ac.nz>
2015-11-13 17:01:46 +1300

    add another test to check error message return

--- ckanext/ezid/tests/test_plugin.py
+++ ckanext/ezid/tests/test_plugin.py
@@ -25,3 +25,20 @@ def test_plugin():
     external_id = instance.get_external_id(context)
     assert 'http://dx.doi.org/' in external_id
     assert instance.get_pretty_name() == 'EZID'
+
+def test_metadata_incomplete():
+    namespace = config.get('ckanext.ezid.namespace')
+    username = config.get('ckanext.ezid.username')
+    password = config.get('ckanext.ezid.password')
+    assert namespace is not None and username is not None and password is not None
+    context = {'package': StubPackageContext()}
+    context['package'].set_dict({
+        'ckan_url': 'https://github.com/UoA-eResearch/ckanext-ezid',
+        'owner_org': 'test_org',
+        'author': '',
+        'title': ''
+    })
+    instance = plugin.EZIDPlugin()
+    external_id = instance.get_external_id(context)
+    assert external_id == \
+        'error: bad request - DOI metadata requirements not satisfied: no creator'
